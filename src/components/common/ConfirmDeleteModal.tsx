7edbbbea20f0c6e6567f59ff03f6f53d11244e90
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-08-15 15:25:30 -0300

    Fixed: Pop Up delete

--- src/components/common/ConfirmDeleteModal.tsx
+++ src/components/common/ConfirmDeleteModal.tsx
@@ -13,24 +13,44 @@ const ConfirmDeleteModal: React.FC<ConfirmDeleteModalProps> = ({
     if (!show) return null;
 
     return (
-        <div className='modal-backdrop fade show' style={{ display: 'block' }}>
-            <div className='modal d-block' tabIndex={-1}>
-                <div className='modal-dialog'>
-                    <div className='modal-content'>
-                        <div className='modal-header'>
-                            <h5 className='modal-title'>{title}</h5>
+        <>
+            {/* Backdrop */}
+            <div
+                className="modal-backdrop fade show"
+                style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    zIndex: 1040
+                }}
+            />
+            
+            {/* Modal */}
+            <div
+                className="modal d-block"
+                tabIndex={-1}
+                style={{
+                    zIndex: 1050
+                }}
+            >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{title}</h5>
                         </div>
-                        <div className='modal-body'>
+                        <div className="modal-body">
                             <p>{message}</p>
                         </div>
-                        <div className='modal-footer'>
-                            <button className='btn btn-secondary' onClick={onCancel}>{cancelText}</button>
-                            <button className='btn btn-danger' onClick={onConfirm}>{confirmText}</button>
+                        <div className="modal-footer">
+                            <button className="btn btn-secondary" onClick={onCancel}>
+                                {cancelText}
+                            </button>
+                            <button className="btn btn-danger" onClick={onConfirm}>
+                                {confirmText}
+                            </button>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
