4fa5dd31f9a0b49c60d27e0509aef4f2b005b495
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-08-20 10:32:09 -0300

    footer alignment design

--- src/components/summary/Footer/Footer.tsx
+++ src/components/summary/Footer/Footer.tsx
@@ -3,12 +3,21 @@ import { getCurrentYear } from '../../../helpers/getCurrentYear';
 
 function Footer(): JSX.Element {
     return (
-        <footer className='text-center py-4 border-t mt-6 text-sm'>
-            <p>© {getCurrentYear()} Designed and developed by ismael Marot. All rights reserved.</p>
-            <nav className='mt-2 space-x-2'>
-                <a href='/terms' className='hover:underline'>Terms of Use</a> |
-                <a href='/legal' className='hover:underline'>Legal</a> | 
-            </nav>
+        <footer className='container py-3 border-top' style={{ fontSize: '.8rem' }}>
+            <div className='row align-items-center text-center text-md-start'>
+                <div className='col-12 col-md-6 mb-2 mb-md-0'>
+                <p className='mb-0'>
+                    © {getCurrentYear()} Designed and developed by Ismael Marot. All rights reserved.
+                </p>
+                </div>
+                <div className='col-12 col-md-6'>
+                    <nav className='d-flex justify-content-center justify-content-md-end'>
+                        <a href='/legal' className='px-2 text-decoration-none'>Legal</a>
+                        <span className='d-none d-md-inline'>|</span>
+                        <a href='/terms' className='px-2 text-decoration-none'>Terms of Use</a>
+                    </nav>
+                </div>
+            </div>
         </footer>
     );
 }
