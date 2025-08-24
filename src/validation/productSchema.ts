f001f2c0315139a77ed71eb673cc8abec03e7432
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-08-24 14:15:46 -0300

    Schema refactored

--- src/validation/productSchema.ts
+++ src/validation/productSchema.ts
@@ -1,24 +1,3 @@
-// import * as yup from 'yup';
-
-// export const productSchema = yup.object({
-//   code: yup.string().optional(),
-//   detail: yup.string().required("Detalle requerido"),
-//   amount: yup.number().typeError("Número válido").positive('> 0').integer("Entero").required(),
-//   cost_price: yup.number().typeError("Número válido").min(0, '>= 0').required(),
-//   sale_price: yup.number().typeError("Número válido").min(0, '>= 0').required(),
-//   usePercentage: yup.boolean(),
-//   percentage: yup
-//     .number()
-//     .typeError("Número válido")
-//     .min(0, '>= 0')
-//     .when('usePercentage', {
-//       is: true,
-//       then: schema => schema.required('Porcentaje requerido'),
-//       otherwise: schema => schema.notRequired(),
-//     }),
-// }).required();
-
-
 import * as yup from 'yup';
 
 export const productSchema = yup.object({
