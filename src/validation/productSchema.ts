1c222ebefeca94be7091ef01b16da62e0b45f982
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-08-24 14:09:02 -0300

    Fix schema in products for % and $ charge

--- src/validation/productSchema.ts
+++ src/validation/productSchema.ts
@@ -1,11 +1,46 @@
+// import * as yup from 'yup';
+
+// export const productSchema = yup.object({
+//   code: yup.string().optional(),
+//   detail: yup.string().required("Detalle requerido"),
+//   amount: yup.number().typeError("Número válido").positive('> 0').integer("Entero").required(),
+//   cost_price: yup.number().typeError("Número válido").min(0, '>= 0').required(),
+//   sale_price: yup.number().typeError("Número válido").min(0, '>= 0').required(),
+//   usePercentage: yup.boolean(),
+//   percentage: yup
+//     .number()
+//     .typeError("Número válido")
+//     .min(0, '>= 0')
+//     .when('usePercentage', {
+//       is: true,
+//       then: schema => schema.required('Porcentaje requerido'),
+//       otherwise: schema => schema.notRequired(),
+//     }),
+// }).required();
+
+
 import * as yup from 'yup';
 
 export const productSchema = yup.object({
   code: yup.string().optional(),
   detail: yup.string().required("Detalle requerido"),
-  amount: yup.number().typeError("Número válido").positive('> 0').integer("Entero").required(),
-  cost_price: yup.number().typeError("Número válido").min(0, '>= 0').required(),
-  sale_price: yup.number().typeError("Número válido").min(0, '>= 0').required(),
+  amount: yup.number()
+    .typeError("Número válido")
+    .positive('> 0')
+    .integer("Entero")
+    .required(),
+  cost_price: yup.number()
+    .typeError("Número válido")
+    .min(0, '>= 0')
+    .required(),
+  sale_price: yup.number()
+    .typeError("Número válido")
+    .min(0, '>= 0')
+    .when('usePercentage', {
+      is: false,
+      then: schema => schema.required('Precio de venta requerido'),
+      otherwise: schema => schema.notRequired()
+    }),
   usePercentage: yup.boolean(),
   percentage: yup
     .number()
