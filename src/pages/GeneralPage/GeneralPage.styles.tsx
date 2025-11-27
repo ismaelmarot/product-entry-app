51340defc22de1c911d7c0886b1519f2fb3ee3d1
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2025-11-27 12:23:07 -0300

    general restyling

---
+++ src/pages/GeneralPage/GeneralPage.styles.tsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const Form = styled('form')`
+    border: 1px solid rgba(153, 161, 175, 1);
+    padding: 1rem;
+    border-radius: .2rem;
+`;
---
+++ src/types/flexValue.type.ts
@@ -0,0 +1,18 @@
+export type FlexDirection =
+    | 'row'
+    | 'column';
+
+export type JustifyContent =
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
+
+export type AlignItems =
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'stretch'
+    | 'baseline';
